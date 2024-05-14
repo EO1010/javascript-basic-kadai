9a7cc3e1b082b2b1824c0536cb0fe2aefa3225ff
EO1010 <156055496+EO1010@users.noreply.github.com>
2024-05-14 11:42:21 +0900

    Add files via upload

---
+++ kadai_mdn.js/mdn.js
@@ -0,0 +1,8 @@
+const date = new Date();
+
+  date.getMonth(),
+  date.getDate(),
+  date.getFullYear(),
+
+
+console.log(date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日' )
